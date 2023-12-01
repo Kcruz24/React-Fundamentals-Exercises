380e51a8afd62f754f52dc489e2a1044c7d046bd
Kevin O. Cruz <kocruz.24@gmail.com>
2023-12-01 14:33:58 -0500

    My solution

--- L3. State Management/2. Passing Data II/Starter/src/App.js
+++ L3. State Management/2. Passing Data II/Starter/src/App.js
@@ -1,108 +1,129 @@
-import "./App.css";
-import logo from "./logo.svg";
+import './App.css';
+import logo from './logo.svg';
 
 // Display a list of movies where each movie contains a list of users that favorited it.
 // For detailed instructions, refer to Instructions.md.
 
 const profiles = [
-  {
-    id: 1,
-    userID: "1",
-    favoriteMovieID: "1",
-  },
-  {
-    id: 2,
-    userID: "2",
-    favoriteMovieID: "1",
-  },
-  {
-    id: 3,
-    userID: "4",
-    favoriteMovieID: "5",
-  },
-  {
-    id: 4,
-    userID: "5",
-    favoriteMovieID: "2",
-  },
-  {
-    id: 5,
-    userID: "3",
-    favoriteMovieID: "5",
-  },
-  {
-    id: 6,
-    userID: "6",
-    favoriteMovieID: "4",
-  },
+	{
+		id: 1,
+		userID: '1',
+		favoriteMovieID: '1',
+	},
+	{
+		id: 2,
+		userID: '2',
+		favoriteMovieID: '1',
+	},
+	{
+		id: 3,
+		userID: '4',
+		favoriteMovieID: '5',
+	},
+	{
+		id: 4,
+		userID: '5',
+		favoriteMovieID: '2',
+	},
+	{
+		id: 5,
+		userID: '3',
+		favoriteMovieID: '5',
+	},
+	{
+		id: 6,
+		userID: '6',
+		favoriteMovieID: '4',
+	},
 ];
 
 const users = {
-  1: {
-    id: 1,
-    name: "Jane Cruz",
-    userName: "coder",
-  },
-  2: {
-    id: 2,
-    name: "Matthew Johnson",
-    userName: "mpage",
-  },
-  3: {
-    id: 3,
-    name: "Autumn Green",
-    userName: "user123",
-  },
-  4: {
-    id: 4,
-    name: "John Doe",
-    userName: "user123",
-  },
-  5: {
-    id: 5,
-    name: "Lauren Carlson",
-    userName: "user123",
-  },
-  6: {
-    id: 6,
-    name: "Nicholas Lain",
-    userName: "user123",
-  },
+	1: {
+		id: 1,
+		name: 'Jane Cruz',
+		userName: 'coder',
+	},
+	2: {
+		id: 2,
+		name: 'Matthew Johnson',
+		userName: 'mpage',
+	},
+	3: {
+		id: 3,
+		name: 'Autumn Green',
+		userName: 'user123',
+	},
+	4: {
+		id: 4,
+		name: 'John Doe',
+		userName: 'user123',
+	},
+	5: {
+		id: 5,
+		name: 'Lauren Carlson',
+		userName: 'user123',
+	},
+	6: {
+		id: 6,
+		name: 'Nicholas Lain',
+		userName: 'user123',
+	},
 };
 
 const movies = {
-  1: {
-    id: 1,
-    name: "Planet Earth 1",
-  },
-  2: {
-    id: 2,
-    name: "Selma",
-  },
-  3: {
-    id: 3,
-    name: "Million Dollar Baby",
-  },
-  4: {
-    id: 4,
-    name: "Forrest Gump",
-  },
-  5: {
-    id: 5,
-    name: "Get Out",
-  },
+	1: {
+		id: 1,
+		name: 'Planet Earth 1',
+	},
+	2: {
+		id: 2,
+		name: 'Selma',
+	},
+	3: {
+		id: 3,
+		name: 'Million Dollar Baby',
+	},
+	4: {
+		id: 4,
+		name: 'Forrest Gump',
+	},
+	5: {
+		id: 5,
+		name: 'Get Out',
+	},
 };
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-      <h2>How Popular is Your Favorite Movie?</h2>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">ReactND - Coding Practice</h1>
+			</header>
+			<h2>How Popular is Your Favorite Movie?</h2>
+			<ol>
+				{Object.entries(movies).map(([key, movie]) => {
+					const usersWhoLikedMovie = profiles
+						.filter(
+							(profile) => String(key) === profile.favoriteMovieID
+						)
+						.map((profile) => users[profile.userID].name);
+
+					const isMovieFavorite = usersWhoLikedMovie.length > 0;
+					return (
+						<li key={key}>
+							<h3>{movie.name}</h3>
+							{isMovieFavorite ? (
+								usersWhoLikedMovie.map((user) => <p>{user}</p>)
+							) : (
+								<p>No one liked this movie</p>
+							)}
+						</li>
+					);
+				})}
+			</ol>
+		</div>
+	);
 };
 
 export default App;
