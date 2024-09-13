550abd8b5459351ac4c2fcf94d6e454340a4d8b2
Kevin O. Cruz <kocruz.24@gmail.com>
2024-09-13 15:58:54 -0400

    add solution for Controlled Components I

--- L3. State Management/4. Controlled Components I/Starter/src/App.js
+++ L3. State Management/4. Controlled Components I/Starter/src/App.js
@@ -1,20 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
 
 const App = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">ReactND - Coding Practice</h1>
-      </header>
-      <div className="container">
-        <input type="text" placeholder="Say Something" />
-        <p className="echo">Echo:</p>
-        <p>This should mirror the text you typed into the input field.</p>
-      </div>
-    </div>
-  );
+	const [echo, setEcho] = useState('');
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<h1 className="App-title">ReactND - Coding Practice</h1>
+			</header>
+			<div className="container">
+				<input
+					type="text"
+					placeholder="Say Something"
+					value={echo}
+					onChange={(e) => setEcho(e.target.value)}
+				/>
+				<p className="echo">Echo: {echo}</p>
+				<p>
+					This should mirror the text you typed into the input field.
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default App;
