8ae89622f4be11180dd9e9d024a99f2cac5f0177
Kevin O. Cruz <kocruz.24@gmail.com>
2024-09-13 14:49:40 -0400

    add solution

--- L3. State Management/3. Managing State/Starter/src/App.js
+++ L3. State Management/3. Managing State/Starter/src/App.js
@@ -1,14 +1,22 @@
-import corgi from "./corgi.jpg";
-import "./App.css";
+import corgi from './corgi.jpg';
+import './App.css';
+import { useState } from 'react';
 
 const App = () => {
-  return (
-    <div className="container">
-      <h2>Like this photo!</h2>
-      <img src={corgi} alt="Corgi"></img>
-      <p>Amount of likes: </p>
-    </div>
-  );
+	const [likes, setLikes] = useState(0);
+
+	const likeHandler = () => {
+		setLikes(likes + 1);
+	};
+
+	return (
+		<div className="container">
+			<h2>Like this photo!</h2>
+			<img src={corgi} alt="Corgi"></img>
+			<p>Amount of likes: {likes}</p>
+			<button onClick={likeHandler}>like me!</button>
+		</div>
+	);
 };
 
 export default App;
